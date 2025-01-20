007cb5d06d4b2015ae42c59572291f045030f63f
Kulvinder Singh <kulvind3r.singh@gmail.com>
2025-01-20 21:40:05 +0530

    Check app presence before brew install

--- mac/configure-host.sh
+++ mac/configure-host.sh
@@ -9,6 +9,15 @@ log() {
 
 log "Starting host configuration"
 
+log "Updating brew"
 brew update
 
-brew install stats rectangle domzilla-caffeine itsycal licecap
+log "Installing apps if absent"
+
+ls /Applications/Stats.app > /dev/null || brew install stats
+ls /Applications/Rectangle.app > /dev/null || brew install rectangle
+ls /Applications/Caffeine.app > /dev/null || brew install domzilla-caffeine
+ls /Applications/Itsycal.app > /dev/null || brew install itsycal 
+ls /Applications/LICEcap.app > /dev/null || brew install licecap
+
+log "Finished installing apps"
